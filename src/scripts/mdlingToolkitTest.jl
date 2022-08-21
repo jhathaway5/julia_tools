8915ddd7c75815614cfb3f8e06f436e9038b6e9b
josh <joshuaphathaway@gmail.com>
2022-08-20 23:40:00 -0500

    adding a project toml and modelling example

---
+++ src/scripts/mdlingToolkitTest.jl
@@ -0,0 +1,32 @@
+# testing out modelling toolkit
+
+
+# If toolbox doesn't exist
+# using Pkg
+# Pkg.add("ModelingToolkit")
+using ModelingToolkit, OrdinaryDiffEq
+
+@parameters t σ ρ β
+@variables x(t) y(t) z(t)
+D = Differential(t)
+
+eqs = [D(D(x)) ~ σ*(y-x),
+       D(y) ~ x*(ρ-z)-y,
+       D(z) ~ x*y - β*z]
+
+@named sys = ODESystem(eqs)
+sys = ode_order_lowering(sys)
+
+u0 = [D(x) => 2.0,
+      x => 1.0,
+      y => 0.0,
+      z => 0.0]
+
+p  = [σ => 28.0,
+      ρ => 10.0,
+      β => 8/3]
+
+tspan = (0.0,100.0)
+prob = ODEProblem(sys,u0,tspan,p,jac=true)
+sol = solve(prob,Tsit5())
+using Plots; plot(sol,vars=(x,y))
